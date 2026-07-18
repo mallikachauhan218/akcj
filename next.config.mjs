9176a79f40b76328a12d6be87c1d3026b816a177
mallikauptechmind <mallikachauhan2118@gmail.com>
2026-07-18 15:35:15 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['xcoder.a2hosted.com'],
-    
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'akcj.vercel.app',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        pathname: '/**', 
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'akcj.vercel.app',
+    },
+  ],
+},
   compiler: {
     // Highlight-start: Enable styled-components support
     styledComponents: true,
